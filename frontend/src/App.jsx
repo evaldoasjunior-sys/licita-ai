9b0f55e4d81c9fc4db4e355dcaad30be042a3f10
Evaldo Antonio Silva Junior <evaldoasjunior@gmail.com>
2026-06-14 09:42:21 -0300

    MVP 0.1 - Primeira versão do LICITA AI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div style={{
+      textAlign: "center",
+      marginTop: "50px",
+      fontFamily: "Arial"
+    }}>
+      <h1>LICITA AI</h1>
+
+      <h3>Inteligência para Licitações</h3>
+
+      <br />
+
+      <button>Nova Oportunidade</button>
+
+      <br /><br />
+
+      <button>Fornecedores</button>
+
+      <br /><br />
+
+      <button>Histórico</button>
+
+      <br /><br />
+
+      <p>Versão MVP 0.1</p>
+    </div>
+  );
+}
+
+export default App;
